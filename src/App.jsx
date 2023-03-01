8d8dfd47829c20d99ee45020e51d4e10dbf7efe1
Abdiel Esteban Alfonso Chávez <isc19350558@gmail.com>
2023-02-28 18:13:17 -0600

    Modifica App y Main para mostrar Hello Lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const nombre = "Abdiel Esteban Alfonso Chávez";
+  return (
+    <div>
+      <h3>Lista de Hobbies de {nombre}</h3>
+      <ol>
+        <li>Jugar videojuegos</li>
+        <li>Leer</li>
+        <li>Ver series y películas</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
